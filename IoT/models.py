264be69c580381a349362bb3d55b77e6e88cf934
DothYu <th2009yu@gmail.com>
2018-07-23 21:45:18 +0800

    更新了数据模型

--- IoT/models.py
+++ IoT/models.py
@@ -12,10 +12,10 @@ class Owner(models.Model):
         return self.name
 
 
-# 状态
+# 大棚状态
 class Status(models.Model):
     """
-    状态
+    大棚状态：正常、警告
     """
     status = models.CharField(max_length=100, primary_key=True)
 
@@ -27,7 +27,7 @@ class Status(models.Model):
 class Area(models.Model):
     """
     编号 [主键]
-    细节说明 [可选]
+    大棚详细说明 [可选]
     经度
     纬度
     状态 [外键]
@@ -40,8 +40,6 @@ class Area(models.Model):
     二氧化碳浓度下限
     连续多少条数据超过阈值开始控制
     """
-    code = ('正常', '警告')
-
     number = models.IntegerField(primary_key=True)
     detail = models.CharField(max_length=1000, blank=True)
     longitude = models.CharField(max_length=100)
@@ -64,13 +62,13 @@ class RaspberryPi(models.Model):
     所属的大棚（区域）[外键]
     """
     number = models.IntegerField(primary_key=True)
-    belongTo = models.ForeignKey(Area, related_name='raspberrypi', on_delete=models.CASCADE)
+    Area_number = models.ForeignKey(Area, related_name='raspberrypi', on_delete=models.CASCADE)
 
 
 # Arduino类型
 class KindOfArduino(models.Model):
     """
-    类型 [主键]
+    类型 [主键]（1.山林 2.水下 3.田野）
     """
     kind = models.CharField(max_length=100, primary_key=True)
 
@@ -81,27 +79,22 @@ class KindOfArduino(models.Model):
 # Arduino
 class Arduino(models.Model):
     """
-    所属的大棚（区域） [外键]
     所属的树莓派 [外键]
     Arduino编号 [主键]
-    Arduino类型（1.山林 2.水下 3.田野）[外键]
+    Arduino类型 [外键]
     """
-    belongToArea = models.ForeignKey(Area, related_name='arduino', on_delete=models.CASCADE)
-    belongToRbp = models.ForeignKey(RaspberryPi, related_name='arduino', on_delete=models.CASCADE)
+    Rbp_number = models.ForeignKey(RaspberryPi, related_name='arduino', on_delete=models.CASCADE)
     number = models.IntegerField(primary_key=True)
     kind = models.ForeignKey(KindOfArduino, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('belongToArea', 'belongToRbp', 'number',)
+        ordering = ('Rbp_number', 'number',)
 
 
 # iot数据
 class Agri(models.Model):
     """
-    1.所属的大棚（区域） [外键]
-    所属的树莓派 [外键]
     所属Arduino编号 [外键]
-    所属Arduino类型（1.山林 2.水下 3.田野）[外键]
     土壤湿度 [可选]
     土壤湿度 [可选]
     土壤盐度 [可选]
@@ -114,12 +107,9 @@ class Agri(models.Model):
     气压 [可选]
     风速 [可选]
     氧气浓度 [可选]
-    17.时间
+    时间
     """
-    belongToArea = models.ForeignKey(Area, related_name='agri', on_delete=models.CASCADE)
-    belongToRbp = models.ForeignKey(RaspberryPi, related_name='agri', on_delete=models.CASCADE)
-    belongToArd = models.ForeignKey(Arduino, on_delete=models.CASCADE)
-    kind = models.ForeignKey(KindOfArduino, related_name='agri', on_delete=models.CASCADE)
+    Ard_number = models.ForeignKey(Arduino, on_delete=models.CASCADE)
     soil_Humidity = models.CharField(max_length=100, blank=True)
     soil_Temp = models.CharField(max_length=100, blank=True)
     soil_Salinity = models.CharField(max_length=100, blank=True)
@@ -134,6 +124,8 @@ class Agri(models.Model):
     O2_Concentration = models.CharField(max_length=100, blank=True)
     created = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ('Ard_number', 'created',)
 
 # 田野
 # class Field(models.Model):
